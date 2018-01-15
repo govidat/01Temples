24429fddb26745262ab23554426645499bb81e70
R Govindarajan <govidat@gmail.com>
2018-01-15 18:03:10 +0530

    Pre refactoring

--- src/components/AppGen3aTemples.vue
+++ src/components/AppGen3aTemples.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="accordionTemples" role="tablist">
+<!-- Have called components at a lower level as the accordion at this level is not functioning -->
     <div class="card" v-for="(item, index) in selTemples">
       <!-- v-for="(item, index) - This index is used to show the first item as show - by default -->
       <div class="card-header" role="tab" :id="'headingTemples'+String(item)">
-        <!-- :id="'headingTemples'+String(item)" - This is used to make the id unique across all components -->
         <h5 class="mb-0">
           <a data-toggle="collapse" :href="'#'+'itemTemple'+String(item)" aria-expanded="true" :aria-controls="'itemTemple'+String(item)">
             <label :for="'headingTemples'+String(item)">
@@ -14,23 +14,16 @@
       </div>
 
       <div :id="'itemTemple'+String(item)" class="collapse" :class="{ show: index===0}" role="tabpanel" :aria-labelledby="'headingTemples'+String(item)" data-parent="#accordionTemples">
-      <!-- <div :id=item+2000 class="collapse show" role="tabpanel" :aria-labelledby=item+2000 data-parent="#accordion"> -->
-        <div class="card-body">
-          Temple details {{ templesDetails.find(itm => itm.Id === item).Detail1 }}
-          <br />
-          Body {{ templesDetails.find(itm => itm.Id === item).Detail2 }}
-        </div>
+        <!-- Show the details for each item -->
+        <z40Tem :details="templesDetails.find(itm => itm.Id === item)"></z40Tem>
       </div>
     </div>
-
   </div>
-
-
-
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import z40Tem from './AppGen3aTemples2.vue';
 
 export default {
   computed: {
@@ -41,6 +34,11 @@ export default {
     // picked: 'filterselected',
     }),
   },
+  components: {
+    // z41Tem,
+    z40Tem
+  },
+
 
 }
 
--- src/components/AppGen3cSongs.vue
+++ src/components/AppGen3cSongs.vue
@@ -44,34 +44,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="card">
-      <div class="card-header" role="tab" id="headingTwo">
-        <h5 class="mb-0">
-          <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Saint details #2
-          </a>
-        </h5>
-      </div>
-      <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordionTemples">
-        <div class="card-body">
-          Collapse 2 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header" role="tab" id="headingThree">
-        <h5 class="mb-0">
-          <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Saint details #3
-          </a>
-        </h5>
-      </div>
-      <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordionTemples">
-        <div class="card-body">
-          Collapse 3 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
